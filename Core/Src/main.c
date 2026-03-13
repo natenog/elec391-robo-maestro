5d9154449a8bc8e367a2fd8a1591156fcaa1b172
natenog <nnog2016@gmail.com>
2026-03-12 18:40:59 -0700

    Increased maxVel, added more targets with shorter idle time in between, and removed commented out MotorControl()

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -61,7 +61,7 @@ uint8_t N = 25;
 float dt = 0.001f;
 
 float vel = 0.0f;
-float maxVel = 25000.0f;
+float maxVel = 30000.0f;
 float maxAccel = 30000.0f;
 
 MotorStruct Motor = {0};
@@ -199,7 +199,7 @@ int main(void)
 		{
 			lastPrint = HAL_GetTick();
 			//printf("Pos=%ld  d=%ld  deriv=%lf  int=%lf  out=%lf  err=%ld  prevErr=%ld\r\n", pos, delta, deriv, integral, output, error, prevError);
-			if (now < 10000) {
+			if (now < 12000) {
 				printf("%ld,%ld,%ld,%lf,%lf,%lf,%lf\r\n", now, pos, subTarget, prop, integral, deriv, output);
 			}
 		}
@@ -214,6 +214,18 @@ int main(void)
 
 	if (now >= 6000) {
 		target = 3000;
+	}
+
+	if (now >= 8000) {
+		target = 2500;
+	}
+
+	if (now >= 9000) {
+		target = 2000;
+	}
+
+	if (now >= 10000) {
+		target = 2750;
 	}
 
     /* USER CODE END WHILE */
@@ -475,28 +487,6 @@ void MotorSetSpeedPercent(float percent) {
     uint32_t speed = (percent / 100.0) * 63999;
     __HAL_TIM_SET_COMPARE(&HTIM_MOTOR, TIM_CHANNEL_1, speed);
 }
-
-/*
-void MotorControl(MotorDirection direction) {
-	switch (direction) {
-	case MOTOR_FORWARD:
-		HAL_GPIO_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, GPIO_PIN_RESET);
-		break;
-	case MOTOR_BACKWARD:
-		HAL_GPIO_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, GPIO_PIN_SET);
-		break;
-	case MOTOR_STOP:
-		HAL_GPIO_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, GPIO_PIN_RESET);
-		break;
-	default:
-		printf("ERROR: Invalid motor control setting. Please specify either MOTOR_FORWARD, MOTOR_BACKWARD, or MOTOR_STOP");
-		exit(EXIT_FAILURE);
-	}
-}
-*/
 
 /* USER CODE END 4 */
 
