d3cbf18f83106bbcadae2aee4b999cf6bee180e4
natenog <nnog2016@gmail.com>
2026-03-17 17:08:09 -0700

    Added angular velocity graph

--- Scripts/graph.py
+++ Scripts/graph.py
@@ -19,12 +19,17 @@ window2 = pg.GraphicsLayoutWidget(title="Subtarget")
 plot2 = window2.addPlot(title="Subtarget")
 curve2 = plot2.plot()
 
+window3 = pg.GraphicsLayoutWidget(title="Velocity")
+plot3 = window3.addPlot(title="Velocity")
+curve3 = plot3.plot()
+
 encoderVals = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 timestamps = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 subTargets = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 props = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 integrals = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 derivs = deque([0]*BUF_LEN, maxlen=BUF_LEN)
+vels = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 outputs = deque([0]*BUF_LEN, maxlen=BUF_LEN)
 
 def update():
@@ -33,12 +38,13 @@ def update():
             line = ser.readline().decode(errors='ignore').strip()
             if not line:
                 break
-            timestamp_str, encoderVal_str, subTarget_str, prop_str, integral_str, deriv_str, output_str = line.split(',')
+            timestamp_str, encoderVal_str, subTarget_str, prop_str, integral_str, deriv_str, vel_str, output_str = line.split(',')
             timestamp = float(timestamp_str)
             encoderVal = int(encoderVal_str)
             subTarget = int(subTarget_str)
             prop = float(prop_str)
             integral = float(integral_str)
+            vel = float(vel_str)
             deriv = float(deriv_str)
             output = float(output_str)
 
@@ -48,6 +54,7 @@ def update():
             props.append(prop)
             integrals.append(integral)
             derivs.append(deriv)
+            vels.append(vel)
             outputs.append(output)
 
         except Exception as e:
@@ -56,9 +63,10 @@ def update():
     if len(timestamps) > 1:
         time0 = timestamps[0]
         timeDelta = [time - time0 for time in timestamps]
-        print("Time:", timestamps[-1] / 1000.0, "|", "Position:", encoderVals[-1], "|", "Subtarget:", subTargets[-1], "|", "Prop:", props[-1], "|", "Int:", integrals[-1], "|", "Deriv:", derivs[-1], "|", "Out:", outputs[-1])
+        print("Time:", timestamps[-1] / 1000.0, "|", "Position:", encoderVals[-1], "|", "Subtarget:", subTargets[-1], "|", "Prop:", props[-1], "|", "Int:", integrals[-1], "|", "Deriv:", derivs[-1], "|", "Vel:", vels[-1], "|", "Out:", outputs[-1])
         curve.setData(timeDelta, list(encoderVals))
         curve2.setData(timeDelta, list(subTargets))
+        curve3.setData(timeDelta, list(vels))
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
@@ -66,4 +74,5 @@ timer.start(20)
 
 window.show()
 window2.show()
+window3.show()
 app.exec()
